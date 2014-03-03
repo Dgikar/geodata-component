0851af4c8130a5b4ace1f059dc5a07648ab3272f
Nil Portugués <nportugues@idodi.eu>
2014-03-03 17:06:32 +0100

    Building the postal code class

--- src/Sonrisa/Component/Geodata/PostalCode.php
+++ src/Sonrisa/Component/Geodata/PostalCode.php
@@ -2,6 +2,54 @@
 
 class PostalCode
 {
+    /**
+     * @var array
+     */
+    protected $country_to_postal = array();
+
+    /**
+     * @var array
+     */
+	protected $postal_to_country = array();
+
+    /**
+     * @var string
+     */
+	protected $config_file = './PostalCode/Resources/config.php';
+
+	/**
+	 *
+	 */
+	public function __construct()
+	{
+		//Load the array from the Resources folder.
+		if(file_exists($this->config_file))
+		{
+			//Build regex for Country to Postal Code
+			$this->country_to_postal = include($this->config_file);
+			$this->formatRegexArray();
+
+			//Calculate the reverse array
+		}
+		else
+		{
+			throw new Exception('Postal Code array not found');
+		}
+
+		//
+	}
+
+	/**
+	 * Converts the array to valid regular expressions
+	 */
+	protected function formatRegexArray()
+	{
+		$patterns = array("#","@","*");
+		$replacements = array( '\d','[a-zA-Z]', '[a-zA-Z0-9]');
+
+  		$this->country_to_postal = str_replace($patterns,$replacements,$this->country_to_postal);
+	}
+
 	/**
 	 * Check if a country has postal codes.
 	 */
