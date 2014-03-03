dd0f998a620ac84890f7705aeed3479c41bea8f1
Nil Portugués <nportugues@idodi.eu>
2014-03-03 17:33:59 +0100

    Working on postal code class

--- src/Sonrisa/Component/Geodata/PostalCode.php
+++ src/Sonrisa/Component/Geodata/PostalCode.php
@@ -25,29 +25,55 @@ class PostalCode
 		//Load the array from the Resources folder.
 		if(file_exists($this->config_file))
 		{
-			//Build regex for Country to Postal Code
-			$this->country_to_postal = include($this->config_file);
-			$this->formatRegexArray();
-
-			//Calculate the reverse array
+			$this->loadConfigFile();
+			$this->buildCountryToPostalRegexArray();
+			$this->buildPostalToCountryRegexArray();
 		}
 		else
 		{
 			throw new Exception('Postal Code array not found');
 		}
+	}
 
-		//
+	/**
+	 * Loads config file to the country to postal array.
+	 */
+	protected function loadConfigFile()
+	{
+		$this->country_to_postal = include($this->config_file);
 	}
 
 	/**
 	 * Converts the array to valid regular expressions
 	 */
-	protected function formatRegexArray()
+	protected function buildCountryToPostalRegexArray()
 	{
 		$patterns = array("#","@","*");
 		$replacements = array( '\d','[a-zA-Z]', '[a-zA-Z0-9]');
 
   		$this->country_to_postal = str_replace($patterns,$replacements,$this->country_to_postal);
+	}
+
+
+	/**
+	 * Calculates the array "postal to country code" based on the "country to postal code" array.
+	 */
+	protected function buildPostalToCountryRegexArray()
+	{
+		foreach($this->country_to_postal as $country => $formats)
+		{
+			foreach($formats as $code)
+			{
+				if(!empty($this->postal_to_country[$code]))
+				{
+					$this->postal_to_country[$code][] = $country;
+				}
+				else
+				{
+					$this->postal_to_country[$code] = array($country);
+				}
+			}
+		}		
 	}
 
 	/**
@@ -76,9 +102,17 @@ class PostalCode
 
 	/**
 	 * Pass a country code and retrieve the country postal codes.
+	 *
+	 * @param string $country_code
+	 * @return array	
 	 */
 	public function countryCodes($country_code)
 	{
-
+		$codes = array();
+		if( array_key_exists(strtoupper($country_code), $this->country_to_postal) )
+		{
+			$codes = $this->country_to_postal[$country_code];
+		}
+		return $codes;
 	}
 }
