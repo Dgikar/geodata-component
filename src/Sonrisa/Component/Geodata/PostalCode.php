d36d0782c15127f84249fe424329f00c8343a109
Nil Portugués <nportugues@idodi.eu>
2014-03-03 16:12:58 +0100

    Building base structure

---
+++ src/Sonrisa/Component/Geodata/PostalCode.php
@@ -0,0 +1,36 @@
+<?php
+
+class PostalCode
+{
+	/**
+	 * Check if a country has postal codes.
+	 */
+	public function isSupported($country_code)
+	{
+
+	}
+
+	/**
+	 * Pass a country code and a postal code. Returns TRUE if valid, FALSE otherwise. 
+	 */
+	public function isValid($country_code,$postal_code)
+	{
+
+	}
+
+	/**
+	 * Pass a postal code and return the matching countries.
+	 */
+	public function matchesWith($postal_code)
+	{
+
+	}
+
+	/**
+	 * Pass a country code and retrieve the country postal codes.
+	 */
+	public function countryCodes($country_code)
+	{
+
+	}
+}
